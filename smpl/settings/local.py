a0200eb2a7b7d17f54f387e95a590ef7ee9dfc07
Sh4reef <shareef.banjar@gmail.com>
2018-02-18 15:31:51 +0300

    update database

--- smpl/settings/local.py
+++ smpl/settings/local.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'smpl.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite',
+        'NAME': 'db.sqlite3',
     }
 }
 
