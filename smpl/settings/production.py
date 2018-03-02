39cf1cee806e80d6a53e728d68f9daad16cd2b11
Sh4reef <shareef.banjar@gmail.com>
2018-03-02 20:49:49 +0300

    remove key

--- smpl/settings/production.py
+++ smpl/settings/production.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wiilsncqp9#9rb#(vsv&ayo^c^1n$y^#ivxm15b($yj_vz!2)!'
+SECRET_KEY = 'secret is secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
